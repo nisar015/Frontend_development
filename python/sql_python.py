197e5efc11fc0395b9c6f41bcb1c003284b3dd5d
nisar015 <nisarachukatla15@gmail.com>
2026-08-13 13:01:00 +0530

    aded

--- python/sql_python.py
+++ python/sql_python.py
@@ -4,6 +4,7 @@ mydb = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="root",
-    password="SqL#@1234"
+    password="SqL#@123"
 )
+# mydb
 print("Connected successfully!")
