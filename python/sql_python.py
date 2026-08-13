7fd6428b5c7350b6869aafb2a5228d8e4d6610fe
nisar015 <nisarachukatla15@gmail.com>
2026-08-13 12:41:47 +0530

    aded

--- python/sql_python.py
+++ python/sql_python.py
@@ -1,2 +1,10 @@
 import mysql.connector
-mydb = mysql.connector.connect(host="127.0.0.1:3306",user="new connection 1",passwd="")
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="new connection 1",
+    password="SqL#@123"
+)
+
+print("Connected successfully!")
