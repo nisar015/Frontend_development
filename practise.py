d6172f1e4db7b73afb762bbd3cc516e7e4b0d8cb
nisar015 <nisarachukatla15@gmail.com>
2026-08-15 13:07:32 +0530

    dic

--- practise.py
+++ practise.py
@@ -60,6 +60,6 @@ print(dic)
 print(dic.get(6))
 print(dic.get(2,"not found"))
 keys=['nisar','ahmmed','ak']
-values=["waste1","waste2","waste3"]
+values=['waste1','waste2','waste3']
 dic=dict(zip(keys,values))
 print(dic["ahmmed"])
